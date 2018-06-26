7746d2ee642933d7a5ec9a292ba22a54dd37eafb
bbwambale <basiyonah033@gmail.com>
2018-06-25 22:01:45 -0500

    WorldCup javaScript file

---
+++ worldCupRecords.js
@@ -0,0 +1,15 @@
+"use strict"
+function myCallToApi(url){
+    fetch(url).then(data => data.json()).
+    then(data => dataGen.next(data))
+}
+
+function* getAllDetails(){
+    console.log('fetching All WorldCup details');
+    const teamInfo = 
+           yield myCallToApi('https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json');
+           console.log(teamInfo);
+}
+
+const dataGen = getAllDetails();
+dataGen.next();
